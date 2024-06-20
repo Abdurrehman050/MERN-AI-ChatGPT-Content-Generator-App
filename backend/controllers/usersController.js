0143a83a30dd22f2fdd17a718c9a1780b1f1fd31
Abdurrehman050 <abdurrehmanfsd07@gmail.com>
2024-06-20 14:52:18 +0500

    error handling

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,49 +1,46 @@
 const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
 
 //------- Registration -------
-const register = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    //Validate
-    if (!username || !email || !password) {
-      res.status(400);
-      throw new Error("Please all fields are required");
-    }
-    // Check the email is taken
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      res.status(400);
-      throw new Error("User Already Exists");
-    }
-    // hash the user password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    // create the user
-    const newUser = new User({
-      username,
-      password: hashedPassword,
-      email,
-    });
-    // add the date trail will end
-    newUser.trailExpires = new Date(
-      new Date().getTime() + newUser.trailPeriod * 24 * 60 * 60 * 1000
-    );
-    // Save the user
-    await newUser.save();
-    res.json({
-      status: true,
-      message: "Registration was successful",
-      user: {
-        username,
-        email,
-      },
-    });
-  } catch (error) {
-    throw new Error(error);
+const register = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  //Validate
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("Please all fields are required");
   }
-};
+  // Check the email is taken
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+  // hash the user password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // create the user
+  const newUser = new User({
+    username,
+    password: hashedPassword,
+    email,
+  });
+  // add the date trail will end
+  newUser.trailExpires = new Date(
+    new Date().getTime() + newUser.trailPeriod * 24 * 60 * 60 * 1000
+  );
+  // Save the user
+  await newUser.save();
+  res.json({
+    status: true,
+    message: "Registration was successful",
+    user: {
+      username,
+      email,
+    },
+  });
+});
 //------- Login -------
 //------- Logout -------
 //------- Profile -------
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const usersRouter = require("./routes/usersRouter");
+const { errorHandler } = require("./middlewares/errorMiddleware");
 require("./utils/connectDB")();
 
 const app = express();
@@ -10,6 +11,9 @@ app.use(express.json()); // pass incoming json or payload
 
 //----Routes-----
 app.use("/api/v1/users", usersRouter);
+
+//-----Error handler middleware
+app.use(errorHandler);
 
 //! Start server
 app.listen(PORT, console.log(`server is running on ${PORT}`));
